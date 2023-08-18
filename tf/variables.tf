19f1862b8b59ae70070ec1cd5abf2e40f1e88c0c
Nick Alteen <ncalteen@github.com>
2023-08-17 22:44:33 -0400

    Comment unused vars

--- tf/variables.tf
+++ tf/variables.tf
@@ -4,25 +4,25 @@
 # composable and reusable.
 # https://developer.hashicorp.com/terraform/language/values/variables
 
-variable "aws_region" {
-  description = "The AWS region to deploy to."
-  type        = string
-  default     = "us-east-1"
-}
+# variable "aws_region" {
+#   description = "The AWS region to deploy to."
+#   type        = string
+#   default     = "us-east-1"
+# }
 
-variable "aws_spot_instance_profile_arn" {
-  description = "The ARN of the IAM instance profile to attach to the spot instances."
-  type        = string
-}
+# variable "aws_spot_instance_profile_arn" {
+#   description = "The ARN of the IAM instance profile to attach to the spot instances."
+#   type        = string
+# }
 
-variable "aws_spot_instance_type" {
-  description = "The type of spot instance to launch."
-  type        = string
-  default     = "t1.micro"
-}
+# variable "aws_spot_instance_type" {
+#   description = "The type of spot instance to launch."
+#   type        = string
+#   default     = "t1.micro"
+# }
 
-variable "aws_spot_instance_max_count" {
-  description = "The maximum number of spot instances to launch."
-  type        = number
-  default     = 1
-}
+# variable "aws_spot_instance_max_count" {
+#   description = "The maximum number of spot instances to launch."
+#   type        = number
+#   default     = 1
+# }
