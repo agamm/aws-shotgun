1567c0603e125131bb802f515949a7b1c2ab4143
Nick Alteen <ncalteen@github.com>
2023-08-17 22:42:33 -0400

    Fix linting errors

--- tf/main.tf
+++ tf/main.tf
@@ -8,7 +8,8 @@ locals {
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/sqs_queue
 resource "aws_sqs_queue" "message_queue" {
   name_prefix                = "message-queue"
-  visibility_timeout_seconds = 90
   receive_wait_time_seconds  = 10
+  sqs_managed_sse_enabled    = true
   tags                       = local.tags
+  visibility_timeout_seconds = 90
 }
