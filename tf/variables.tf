e300d6d3aa9cc029b8743a0bfec95be5a5c5b6f3
Nick Alteen <ncalteen@github.com>
2023-08-19 00:20:43 -0400

    Comment/remove unused variables

--- tf/variables.tf
+++ tf/variables.tf
@@ -4,17 +4,11 @@
 # composable and reusable.
 # https://developer.hashicorp.com/terraform/language/values/variables
 
-variable "aws_region" {
-  description = "The AWS region to deploy to."
-  type        = string
-  default     = "us-east-1"
-}
-
-variable "aws_spot_instance_ami" {
-  description = "The AMI to use for the spot instances."
-  type        = string
-  default     = "ami-08a52ddb321b32a8c"
-}
+# variable "aws_region" {
+#   description = "The AWS region to deploy to."
+#   type        = string
+#   default     = "us-east-1"
+# }
 
 variable "aws_spot_instance_bid_usd" {
   description = "The maximum bid price per hour for the spot instances."
@@ -33,14 +27,14 @@ variable "aws_spot_instance_type" {
   default     = "t1.micro"
 }
 
-variable "aws_spot_instance_max_count" {
-  description = "The maximum number of spot instances to launch."
-  type        = number
-  default     = 1
-}
+# variable "aws_spot_instance_max_count" {
+#   description = "The maximum number of spot instances to launch."
+#   type        = number
+#   default     = 1
+# }
 
-variable "aws_sqs_batch_size" {
-  description = "The maximum number of messages to send to the SQS queue at once."
-  type        = number
-  default     = 10
-}
+# variable "aws_sqs_batch_size" {
+#   description = "The maximum number of messages to send to the SQS queue at once."
+#   type        = number
+#   default     = 10
+# }
