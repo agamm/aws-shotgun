fcd894f7fa77d5c28c313ed5f27bae56e4dc9a14
chrome99 <efeldman207@gmail.com>
2023-09-11 15:21:40 +0300

    SQS polling, fetching, and uploading to S3 are done in batch

--- src/consumer/index.js
+++ src/consumer/index.js
@@ -1,23 +1,47 @@
-import fetch from 'node-fetch'
 import { writeOutput } from '../utils/writeOutput.js'
+import { fetchWithTimeout } from '../utils/consumer.js'
+
+const FETCH_TIMEOUT = 10000;
 
 /**
- * This script is the handler that performs any manipulation of the message
- * before it is uploaded to Amazon S3. It must return the string data that will
- * be written to the S3 bucket.
+ * This script is an example of a handler function that sends an HTTP request to the url
+ * and uploads the result to Amazon S3. You can change it to anything you want ;)
  *
- * @param {object} message The SQS message body as a JSON object
+ * @param {object} messages The SQS messages batch as an array with JSON objects
  * @return {void}
  */
-export async function handler(message) {
-  // Make the request
-  const response = await fetch(message.url)
 
-  // Get the response body as JSON
-  const body = await response.json()
+export async function handler(messages) {
+  try {
+    // running multiple asynchronous tasks
+    const responses = await Promise.allSettled(
+      // mapping messages from producer to return promises array
+      messages.map(async message => {
+        const parsedMessage = JSON.parse(message.Body);
+        const url = parsedMessage.url;
+        const urlWithProtocol = url.startsWith("http") ? url : `http://${url}`;
 
-  // (TODO) Add your processing logic here...
+        console.log("Handling url: ", urlWithProtocol);
+        return await fetchWithTimeout(urlWithProtocol, FETCH_TIMEOUT);
+      })
+    )
+    
+    // handling aggregated results
+    console.log("Responses are back!");
+    const okResponses = responses
+    .filter((res) => {
+      if (res.status === "fulfilled") {
+        console.log('Response OK from ', res.value.url);
+        return res;
+      }
+      else { console.error(res.reason) }
+    })
+    .map(res => res.value);
 
-  // Write the output to S3
-  await writeOutput(message, body)
+    // upload to s3 bucket
+    await writeOutput(okResponses);
+
+  } catch (error) {
+    console.error('An error occurred:', error)
+  }
 }
